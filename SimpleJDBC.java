8619840bd4a479c19d4b92913ea3ace221aafc53
p-e-andrews <90933694+p-e-andrews@users.noreply.github.com>
2022-11-22 14:24:19 -0500

    Add files via upload

---
+++ SimpleJDBC.java
@@ -0,0 +1,34 @@
+package edu.waketech.csc251.validate.mysql;
+
+import java.sql.*;
+
+public class SimpleJDBC {
+	public static void main(String[] args)
+			throws SQLException, ClassNotFoundException {
+		// Load the JDBC driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Driver loaded");
+
+		// Connect to a database
+		Connection connection = DriverManager.getConnection
+				("jdbc:mysql://localhost/javabook", "scott", "tiger");
+		System.out.println("Database connected");
+
+		// Create a statement
+		Statement statement = connection.createStatement();
+
+		// Execute a statement
+		ResultSet resultSet = statement.executeQuery
+				("select firstName, mi, lastName from Student where lastName "
+						+ " = 'Smith'");
+
+		// Iterate through the result and print the student names
+		while (resultSet.next())
+			System.out.println(resultSet.getString(1) + "\t" +
+					resultSet.getString(2) + "\t" + resultSet.getString(3));
+
+		// Close the connection
+		connection.close();
+		System.out.println("Connection closed");
+	}
+}
